85308037133c44b999b2cc2098e499877757b389
Andrey Burlakov <andrburl2@yandex.ru>
2022-03-18 17:32:19 +0300

    Создана форма в корзине. Простая валидация для формы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -9,7 +10,6 @@ import { ProductComponent } from './product/product.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -18,17 +18,16 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductComponent,
     ProductDetailsComponent,
     TopBarComponent,
-    CartComponent,
-    ShippingComponent
+    CartComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   providers: [],
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms'; 
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -8,8 +10,33 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items = this.cartService.getItems();
+  shippingCosts = this.cartService.getShippingPrices();
 
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({
+    shipping: new FormControl('', Validators.required),
+    name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
+    address: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(40)])
+  })
 
-  ngOnInit(): void {}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.checkoutForm.controls['shipping'].valueChanges.subscribe(() => {
+      this.calculateCost();
+    });
+  }
+
+  submitForm(): void {
+    console.log({ ...this.checkoutForm.value, products: this.items });
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+
+  calculateCost(): number {
+    return this.items.reduce((sum, el) => sum + el.price, 0) + Number(this.checkoutForm.controls['shipping'].value)
+  }
 }
